fd502f8df6cb43e0084dff5166d130ef3e7b4382
Elisenda <elisenda.monserrat@gmail.com>
2021-03-15 15:18:58 +0100

    adds assign control keys function to make the player move in all directions

--- js/chef.js
+++ js/chef.js
@@ -3,75 +3,39 @@
 class Chef {
     constructor(maxRows, maxColumns) {
         this.initialPosition = {
-            row: 25, 
-            column: 49, 
+            column: 25, 
+            row: 49, 
         };
-        this.maxColumns = maxRows;
-        this.maxRows = maxColumns;
-        this.direction = "up";
+        this.maxColumns = maxColumns;
+        this.maxRows = maxRows;
         this.score = 0;
     }
 
-    _move() {
+    goUp() {
+        this.initialPosition.row = (this.initialPosition.row - 1 + this.maxRows) % this.maxRows;
+    }
 
-        switch (this.direction) {
-            case "up":
-                this.initialPosition = {
-                    row: (this.initialPosition.row + 1) % this.maxRows,
-                    column: this.initialPosition.column,
-                }
-                break;
+    goDown() {
+        this.initialPosition.row = (this.initialPosition.row + 1) % this.maxRows;
+    }
 
-            case "down":
-                this.initialPosition = {
-                    row: (this.initialPosition.row - 1) % this.maxRows,
-                    column: this.initialPosition.column,
-                }
-                break;
-            case "left":
-                this.initialPosition = {
-                    row: this.initialPosition.row,
-                    column: (this.initialPosition.column -1 + this.maxColumns) % this.maxColumns,
-                }
-                break;
-            case "right":
-                this.initialPosition = {
-                    row: this.initialPosition.row,
-                    column: (this.initialPosition.column + 1) % this.maxColumns,
-                }
-                break;
-            default:
-                break;
-        }
+    goLeft() {
+        this.initialPosition.column = (this.initialPosition.column - 1 + this.maxColumns) % this.maxColumns;
+    }
+
+    goRight() {
+        this.initialPosition.column = (this.initialPosition.column + 1) % this.maxColumns;
+    }
+    
+    collidesWithObstacle() {
 
     }
 
-    _goUp() {
-        
+    takesIngredient() {
 
     }
 
-    _goDown() {
-
-    }
-
-    _goLeft() {
-
-    }
-
-    _goRight() {
-
-    }
-
-    _collidesWithObstacle() {
-
-    }
-
-    _takesIngredient() {
-
-    }
-
-    _scoreUp() {
+    scoreUp() {
         this.score += 10;
     }
 }
--- js/game.js
+++ js/game.js
@@ -15,8 +15,8 @@ class Game {
     _drawChef() {
         this.ctx.fillStyle = "red";
         this.ctx.fillRect(
-            this.chef.initialPosition.row * this.maxCells,
             this.chef.initialPosition.column * this.maxCells,
+            this.chef.initialPosition.row * this.maxCells,
             8,
             8
         )
@@ -24,31 +24,41 @@ class Game {
 
     _assignControlsToKeys() {
         document.addEventListener("keydown", (event) => {
+            //As the player moves the Chef it's initial position gets updated & printed again
             switch (event.code) {
                 case "ArrowUp":
-                    
+                    this.chef.goUp();
                     break;
-            
+                case "ArrowDown":
+                    this.chef.goDown();
+                    break;
+                case "ArrowLeft":
+                   this.chef.goLeft();
+                    break;
+                case "ArrowRight":
+                   this.chef.goRight();
+                    break;
                 default:
                     break;
             }
         })
     }
 
-
-
     _clean() {
-
+        this.ctx.clearRect(0,0, 500, 500);
     }
 
     _update() {
+        this._clean();
+        this._drawChef();
+        window.requestAnimationFrame(this._update.bind(this));
         
     }
 
     _start() {
+       this._assignControlsToKeys();
        this._drawChef();
+       window.requestAnimationFrame(this._update.bind(this));
     }
-
-
 
 }
