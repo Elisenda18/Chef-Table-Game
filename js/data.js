00b8d23891305cbecd43821b825bf5620e88d3eb
Elisenda <elisenda.monserrat@gmail.com>
2021-03-16 20:51:21 +0100

    adds data script for ingredients

--- js/data.js
+++ js/data.js
@@ -3,46 +3,88 @@
 const dataIngredients = [
     {
       name: "bread",
-      img: "./img/bread-min.png"
+      img: "./img/bread-min.png",
+      column: Math.floor(Math.random() * 500),
+      row: Math.floor(Math.random() * 500),
+      width: 10,
+      height: 10
     },
     {
         name: "cheese",
         img: "./img/cheese-min.png",
+        column: Math.floor(Math.random() * 500),
+        row: Math.floor(Math.random() * 500)
     },
     {
       name: "chooping board",
-      img: "./img/choppingboard-min.png"
+      img: "./img/choppingboard-min.png",
+      column: Math.floor(Math.random() * 500),
+      row: Math.floor(Math.random() * 500),
+      width: 10,
+      height: 10
     },
     {
       name: "cookbook",
-      img: "./img/cookbook-min.png"
+      img: "./img/cookbook-min.png",
+      column: Math.floor(Math.random() * 500),
+      row: Math.floor(Math.random() * 500),
+      width: 10,
+      height: 10
     },
     {
         name: "milk",
-        img: "./img/milk-min.png"
+        img: "./img/milk-min.png",
+        column: Math.floor(Math.random() * 500),
+        row: Math.floor(Math.random() * 500),
+        width: 10,
+        height: 10
     },
     {
         name: "mushrooms",
-        img: "./img/mushrooms-min.png"
+        img: "./img/mushrooms-min.png",
+        column: Math.floor(Math.random() * 500),
+        row: Math.floor(Math.random() * 500),
+        width: 10,
+        height: 10
     },
     {
         name: "pan",
-        img: "./img/pan-min.png"
+        img: "./img/pan-min.png",
+        column: Math.floor(Math.random() * 500),
+        row: Math.floor(Math.random() * 500),
+        width: 10,
+        height: 10
     },
     {
         name: "pineapple",
-        img: "./img/pineapple-min.png"
+        img: "./img/pineapple-min.png",
+        column: Math.floor(Math.random() * 500),
+        row: Math.floor(Math.random() * 500),
+        width: 10,
+        height: 10
     },
     {
         name: "salt & pepper",
-        img: "./img/salt&pepper-min.png"
+        img: "./img/salt&pepper-min.png",
+        column: Math.floor(Math.random() * 500),
+        row: Math.floor(Math.random() * 500),
+        width: 10,
+        height: 10
     },
     {
         name: "spicy",
-        img: "./img/spicy-min.png"
+        img: "./img/spicy-min.png",
+        column: Math.floor(Math.random() * 500),
+        row: Math.floor(Math.random() * 500),
+        width: 10,
+        height: 10
     },
     {
         name: "tool",
-        img: "./img/tool-min.png"
+        img: "./img/tool-min.png",
+        column: Math.floor(Math.random() * 500),
+        row: Math.floor(Math.random() * 500),
+        width: 10,
+        height: 10
     }
 ];
