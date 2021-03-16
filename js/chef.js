e4d7ca3fe69ddf39d8a394f26811c059634ecc40
Elisenda <elisenda.monserrat@gmail.com>
2021-03-16 20:53:13 +0100

    refactors takesIngredient function, there is a bug tho

--- js/chef.js
+++ js/chef.js
@@ -1,45 +1,45 @@
 'use strict';
 
 class Chef {
-    constructor(maxRows, maxColumns) {
+    constructor() {
         this.initialPosition = {
-            column: 25, 
-            row: 49, 
+            column: 250, 
+            row: 490, 
         };
-        this.maxColumns = maxColumns;
-        this.maxRows = maxRows;
+        //this.maxColumns = maxColumns;
+        //this.maxRows = maxRows;
         this.score = 0;
         this.currentPosition = {...this.initialPosition};
     }
 
     goUp() {
-        this.currentPosition.row = (this.currentPosition.row - 1 + this.maxRows) % this.maxRows;
-        console.log(this.currentPosition);
+        this.currentPosition.row = (this.currentPosition.row - 5 + 500) % 500;
     }
 
     goDown() {
-        this.currentPosition.row = (this.currentPosition.row + 1) % this.maxRows;
-        console.log(this.currentPosition);
+        this.currentPosition.row = (this.currentPosition.row + 5) % 500;
     }
 
     goLeft() {
-        this.currentPosition.column = (this.currentPosition.column - 1 + this.maxColumns) % this.maxColumns;
-        console.log(this.currentPosition);
-
+        this.currentPosition.column = (this.currentPosition.column - 5 + 500) % 500;
     }
 
     goRight() {
-        this.currentPosition.column = (this.currentPosition.column + 1) % this.maxColumns;
-        console.log(this.currentPosition);
-
+        this.currentPosition.column = (this.currentPosition.column + 5) % 500;
     }
     
     collidesWithKnifes(knife) {
         return this.currentPosition.row === knife.row && this.currentPosition.column === knife.column;
     }
 
-    takesIngredient(ingredient) {
-        return this.currentPosition.row === ingredient.row && this.currentPosition.column === ingredient.column;
+    takesIngredient(chef, ingredient) {
+        return !(
+            ((chef.row + ingredient.height) < (ingredient.row)) ||
+            (chef.row > (ingredient.row + ingredient.height)) ||
+            ((chef.column + chef.width) < ingredient.column) ||
+            (chef.column > (ingredient.column + ingredient.width))
+        ); 
+        //There is a bug in the chef.column, it "collides" when moving the column up & down
     }
 
     scoreUp() {
